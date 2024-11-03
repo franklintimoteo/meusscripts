26b2d592490739e371a5cb1c332457730ea67777
Franklin Timóteo <franklindev@disroot.org>
2024-11-03 14:17:38 -0300

    Usa nome uma variável para rastrear o nome do arquivo de backup

--- backup_selfhosted/backup_selfhosted.sh
+++ backup_selfhosted/backup_selfhosted.sh
@@ -6,25 +6,25 @@
 #
 # Versão 1.0: crie um arquivo de backup para enviar ao GDriver
 # Data: 03/11/2024
-# Autor: Franklin Timóteo dos Santos <franklindev_scripts@disroot.org>
+# Autor: Franklin Timóteo dos Santos <franklindev+scripts@disroot.org>
 # Licença: GNU/PL v3.0
 
 WORKDIR=/root
-
+FILENAME_BASE=backup
 # Copia configurações 
 cp -r /DATA ${WORKDIR}/DATA-BACKUP
 
 # Comprime todos diretórios
-tar -zcf ${WORKDIR}/bkp.tar.gz ${WORKDIR}/DATA-BACKUP
+tar -zcf ${WORKDIR}/${FILENAME_BASE}.tar.gz ${WORKDIR}/DATA-BACKUP
 
 # Criptografa com a chave pública do GPG
 # lembrar de confiar na chave pública
 # caso contrário o gpg pedirá confirmação
-gpg --batch -r franklindev@disroot.org --encrypt ${WORKDIR}/bkp.tar.gz
+gpg --batch -r franklindev@disroot.org --encrypt ${WORKDIR}/${FILENAME_BASE}.tar.gz
 
 # envia o arquivo comprimido e criptografado para o google driver
-rclone copy ${WORKDIR}/bkp.tar.gz.gpg remote:backups-selfhosted
+rclone copy ${WORKDIR}/${FILENAME_BASE}.tar.gz.gpg remote:backups-selfhosted
 
 # faz o mesmo com o checksum
-sha512sum ${WORKDIR}/bkp.tar.gz.gpg > ${WORKDIR}/checksum.sha512
+sha512sum ${WORKDIR}/${FILENAME_BASE}.tar.gz.gpg > ${WORKDIR}/checksum.sha512
 rclone copy ${WORKDIR}/checksum.sha512 remote:backups-selfhosted
