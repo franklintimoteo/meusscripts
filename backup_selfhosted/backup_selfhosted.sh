093a3f6b475e8260608a2585addd9f02090c5fcf
Franklin Timóteo <franklindev@disroot.org>
2024-11-03 14:49:32 -0300

    Evita reusar arquivos antigos enviando para o /tmp

--- backup_selfhosted/backup_selfhosted.sh
+++ backup_selfhosted/backup_selfhosted.sh
@@ -11,6 +11,13 @@
 
 WORKDIR=/root
 FILENAME_BASE=backup
+
+# Caso o diretório de configurções já exista, envia para o tmp
+[[ -e ${WORKDIR}/DATA-BACKUP ]] && mv ${WORKDIR}/DATA-BACKUP /tmp
+
+# tenta copiar arquivos antigos para o tmp
+cp ${WORKDIR}/${FILENAME_BASE}.tar.{gz,gz.gpg} /tmp
+
 # Copia configurações 
 cp -r /DATA ${WORKDIR}/DATA-BACKUP
 
